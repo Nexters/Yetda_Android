02213ccaaa806195bce31f0ad5525d022fb8bb44
Jongkook <conquerex@gmail.com>
2020-01-07 21:27:49 +0900

    빠이야베이스 테스트. 됬다아아아ㅏㅏ

--- app/src/main/java/com/nexters/yetda/android/MainActivity.kt
+++ app/src/main/java/com/nexters/yetda/android/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.nexters.yetda.android
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Access a Cloud Firestore instance from your Activity
+        val db = FirebaseFirestore.getInstance()
+
+        db.collection("presents")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d(TAG, "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents.", exception)
+            }
     }
 }
