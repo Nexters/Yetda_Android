9e7bdecdc2418797b557aa2757b9a7128bae6eb6
Jongkook <conquerex@gmail.com>
2020-01-07 21:13:02 +0900

    빠이야베이스 세팅 중

--- app/src/main/java/com/nexters/yetda/android/MainActivity.kt
+++ app/src/main/java/com/nexters/yetda/android/MainActivity.kt
@@ -1,19 +1,12 @@
 package com.nexters.yetda.android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.firebase.database.FirebaseDatabase
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Write a message to the database
-        val database = FirebaseDatabase.getInstance()
-        val myRef = database.getReference("message")
-
-        myRef.setValue("Hello, World!")
     }
 }
