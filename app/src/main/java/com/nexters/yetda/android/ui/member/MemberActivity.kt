b51009ff7430948d789272840c54a3af7464704f
Jongkook <barley.son@Jongkooks-MacBook-Pro.local>
2021-09-30 20:14:28 +0900

    sample code

--- app/src/main/java/com/nexters/yetda/android/ui/member/MemberActivity.kt
+++ app/src/main/java/com/nexters/yetda/android/ui/member/MemberActivity.kt
@@ -1,8 +1,10 @@
 package com.nexters.yetda.android.ui.member
 
+import android.widget.Toast
 import com.nexters.yetda.android.R
 import com.nexters.yetda.android.base.BaseActivity
 import com.nexters.yetda.android.databinding.ActivityNameBinding
+import kotlinx.android.synthetic.main.activity_member.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class MemberActivity : BaseActivity<ActivityNameBinding, MemberViewModel>() {
@@ -19,6 +21,14 @@ class MemberActivity : BaseActivity<ActivityNameBinding, MemberViewModel>() {
     }
 
     override fun initViewFinal() {
-        //
+        // www.naver.com 웹뷰 + 터치 이벤트(클릭 이벤트)
+
+        imageMemberYena.setOnClickListener {
+            Toast.makeText(
+                this,
+                "예나 이미지를 터치 했다",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
