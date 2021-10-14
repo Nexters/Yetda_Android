d936c096e98e0ebc42f2f3d939827de975f28660
Yenowme <76904042+Yenowme@users.noreply.github.com>
2021-10-14 21:30:51 +0900

    feat: member sns link

--- app/src/main/java/com/nexters/yetda/android/ui/member/MemberActivity.kt
+++ app/src/main/java/com/nexters/yetda/android/ui/member/MemberActivity.kt
@@ -2,6 +2,7 @@ package com.nexters.yetda.android.ui.member
 
 import android.content.Intent
 import android.net.Uri
+import android.view.View
 import android.widget.Toast
 import com.nexters.yetda.android.R
 import com.nexters.yetda.android.base.BaseActivity
@@ -20,19 +21,21 @@ class MemberActivity : BaseActivity<ActivityNameBinding>() {
         //
     }
 
-    override fun initViewFinal() {
-        // www.naver.com 웹뷰 + 터치 이벤트(클릭 이벤트)
-
-        imageMemberJisu.setOnClickListener {
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/xeesoxee/?hl=ko"))
+    fun setSnsListener(member: View, snsUri: String) {
+        member.setOnClickListener {
+            val intent =
+                Intent(Intent.ACTION_VIEW, Uri.parse(snsUri))
             startActivity(intent)
         }
-        imageMemberYena.setOnClickListener {
-            Toast.makeText(
-                this,
-                "예나 이미지를 터치 했다",
-                Toast.LENGTH_SHORT
-            ).show()
-        }
+    }
+
+    override fun initViewFinal() {
+        setSnsListener(imageMemberJisu, "https://www.facebook.com/jisookim1993")
+        setSnsListener(imageMemberKook, "https://instagram.com/bingocake")
+        setSnsListener(imageMemberHeeju, "https://instagram.com/meiyoubella00/")
+        setSnsListener(imageMemberGong, "https://instagram.com/xeesoxee/?hl=ko")
+        setSnsListener(imageMemberDohyun, "https://www.naver.com")
+        setSnsListener(imageMemberSuhyun, "https://instagram.com/dev_suhyun/")
+        setSnsListener(imageMemberYena, "https://instagram.com/d.nowme/")
     }
 }
