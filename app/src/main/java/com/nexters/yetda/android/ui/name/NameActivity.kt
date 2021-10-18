acb6f002e775f5e6c0ede99cd8640aa3ca4f32f6
ellen.youn <ellen.youn@kakaocorp.com>
2021-10-18 21:22:14 +0900

    [navigation 제거] 5-1. name 변경

--- app/src/main/java/com/nexters/yetda/android/ui/name/NameActivity.kt
+++ app/src/main/java/com/nexters/yetda/android/ui/name/NameActivity.kt
@@ -4,12 +4,13 @@ import android.content.Intent
 import androidx.lifecycle.Observer
 import com.nexters.yetda.android.R
 import com.nexters.yetda.android.base.BaseActivity
+import com.nexters.yetda.android.base.BaseFragment
 import com.nexters.yetda.android.databinding.ActivityNameBinding
 import com.nexters.yetda.android.ui.gender.GenderActivity
 import org.koin.android.viewmodel.ext.android.viewModel
 
 
-class NameActivity : BaseActivity<ActivityNameBinding>() {
+class NameActivity : BaseFragment<ActivityNameBinding>() {
     override val layoutResourceId = R.layout.activity_name
     val viewModel: NameViewModel by viewModel()
 
@@ -26,12 +27,12 @@ class NameActivity : BaseActivity<ActivityNameBinding>() {
         //        Crashlytics.getInstance().crash() // Force a crash
 
         viewModel.startNextActivityEvent.observe(this, Observer {
-            val intent = Intent(this, GenderActivity::class.java)
+            val intent = Intent(context, GenderActivity::class.java)
             intent.putExtra("NAME", viewModel.name.value)
             startActivity(intent)
         })
         viewModel.backBeforeActivityEvent.observe(this, Observer {
-            finish()
+            requireActivity().finish()
         })
     }
 
