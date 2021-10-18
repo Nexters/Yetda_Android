ba9420fcd478fdbcd049257213109a9dca45dcae
ellen.youn <ellen.youn@kakaocorp.com>
2021-10-18 20:55:32 +0900

    [navigation 제거] 3. MainActivity 추가

---
+++ app/src/main/java/com/nexters/yetda/android/ui/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.nexters.yetda.android.ui
+
+import com.nexters.yetda.android.R
+import com.nexters.yetda.android.base.BaseActivity
+import com.nexters.yetda.android.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity<ActivityMainBinding>() {
+    override val layoutResourceId: Int = R.layout.activity_main
+
+    override fun initViewStart() {}
+
+    override fun initDataBinding() {}
+
+    override fun initViewFinal() {}
+}
